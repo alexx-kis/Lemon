9ee3328ca50ecab41405e3f80caf5d4587cc2c4e
alex__kis <fenaethylaamiinloever@gmail.com>
2023-05-18 23:13:27 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -5,36 +5,14 @@ $(function () {
 		infinite: true,
 		dragable: false,
 		waitForAnimate: false,
-		// responsive:
-		// 	[
-		// 		{
-		// 			breakpoint: 1100,
-		// 			settings: {
-		// 				slidesToShow: 3,
-		// 			}
-		// 		},
-		// 		{
-		// 			breakpoint: 750,
-		// 			settings: {
-		// 				slidesToShow: 2,
-		// 			}
-		// 		},
-		// 		{
-		// 			breakpoint: 550,
-		// 			settings: {
-		// 				slidesToShow: 1,
-		// 				dragable: true,
-		// 			}
-		// 		},
-		// 	],
 		arrows: false,
 		dots: true,
 		appendDots: ('.slider__dots'),
 		waitForAnimate: false,
 	})
-	$('.burger, .overlay, .menu ').on('click', function (e) {
-		e.preventDefault()
-		$('.menu').toggleClass('menu--open')
+	$('.burger, .overlay, .header a').on('click', function (e) {
+		// e.preventDefault()
+		$('.header').toggleClass('header--open')
 		$('.overlay').toggleClass('overlay--show')
 		$('.burger').toggleClass('burger--open')
 	})
